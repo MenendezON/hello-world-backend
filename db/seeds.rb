1fd9b4bdec1bce6c51a08e80da44ac755c67f91f
MenendezON <menendezon@gmail.com>
2023-12-04 12:30:12 +0000

    Create the migration file, message

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Message.create(greeting: 'Hello, world!')
+Message.create(greeting: 'Hi, there!')
+Message.create(greeting: 'Messages, user!')
+Message.create(greeting: 'Howdy, partner!')
+Message.create(greeting: 'Salutations!')
